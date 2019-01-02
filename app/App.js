ff2e4dd196a8a911d6e2c543b4e9d2a743a8f326
HL <36248745+honestbonsai@users.noreply.github.com>
2019-01-02 12:12:01 -0500

    Init

---
+++ app/App.js
@@ -0,0 +1,63 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+import ReadString from "./ReadString";
+import SetString from "./SetString";
+
+type Props = {};
+export default class App extends Component<Props> {
+  state = { loading: true, drizzleState: null };
+
+  componentDidMount() {
+    const { drizzle } = this.props;
+
+    this.unsubscribe = drizzle.store.subscribe(() => {
+      const drizzleState = drizzle.store.getState();
+
+      if (drizzleState.drizzleStatus.initialized) {
+        this.setState({ loading: false, drizzleState });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        {this.state.loading ? (
+          <Text>Loading...</Text>
+        ) : (
+          <View>
+            <ReadString
+              drizzle={this.props.drizzle}
+              drizzleState={this.state.drizzleState}
+            />
+            <SetString
+              drizzle={this.props.drizzle}
+              drizzleState={this.state.drizzleState}
+            />
+          </View>
+        )}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  }
+});
---
+++ index.js
@@ -0,0 +1,17 @@
+/** @format */
+import "./shims";
+import { AppRegistry } from "react-native";
+import App from "./app/App";
+import { name as appName } from "./app.json";
+
+import React from "react";
+import { Drizzle, generateStore } from "drizzle";
+import MyStringStore from "./build/contracts/MyStringStore.json";
+
+const options = {
+  contracts: [MyStringStore]
+};
+const drizzleStore = generateStore(options);
+const drizzle = new Drizzle(options, drizzleStore);
+
+AppRegistry.registerComponent(appName, () => () => <App drizzle={drizzle} />);
